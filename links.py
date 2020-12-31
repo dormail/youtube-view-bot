ddbfa94e697c9f8689f67557d94d236ecd5fe21e
Matthias Maile <matthias.maile@udo.edu>
2020-12-31 16:08:04 +0100

    template file for links

---
+++ links.py
@@ -0,0 +1,6 @@
+""" links.py 
+here we can define the links the bot should iterate through
+"""
+
+links = ['https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        'https://www.youtube.com/watch?v=VbUuB1aN2DA']
